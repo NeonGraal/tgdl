0b13c4c95042c1a2be7df4184e0409a336cb9b77
NeonGraal <github@scifi.geek.nz>
2019-04-19 22:48:42 +1200

    Add parsing benchmarks

--- src/benchmark/Main.hs
+++ src/benchmark/Main.hs
@@ -1,6 +1,18 @@
 -- You can benchmark your code quickly and effectively with Criterion. See its
 -- website for help: <http://www.serpentine.com/criterion/>.
+import TGDL.Parser
+
+import Criterion.Types
 import Criterion.Main
 
+parseBench :: String -> Benchmark
+parseBench t = bench benchDescr $ whnf benchParse ()
+    where
+        benchDescr = "parse '" ++ t ++ "'"
+        benchParse = parseTGDL benchDescr t
+
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main = defaultMainWith config [bgroup "Parsing" [parseBench t | t <- parseBenchmarks]]
+    where
+        config = defaultConfig { timeLimit = 2 }
+        parseBenchmarks = [".node1", "-node2", ".node1-node2", "node1-node2"]
