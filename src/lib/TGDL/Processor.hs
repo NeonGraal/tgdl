6d4fa53be9fbd4428083107c7fdbc73bb9fa9691
NeonGraal <github@scifi.geek.nz>
2019-04-21 13:40:27 +1200

    Draw something using Diagrams

--- src/lib/TGDL/Processor.hs
+++ src/lib/TGDL/Processor.hs
@@ -1,6 +1,12 @@
 -- | The TGDL Processor
 module TGDL.Processor (main) where
 
+import Diagrams.Prelude
+import Diagrams.Backend.Rasterific.CmdLine
+
+fromFile :: FilePath -> IO (Diagram B)
+fromFile f = return $ text f <> ellipseXY 10 2 # lw 0.1
+
 -- | TGDL Processor main function
 main :: IO ()
-main = return ()
+main = mainWith fromFile
